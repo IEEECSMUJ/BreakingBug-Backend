4a5d73917d2d2c5d610cae125524f2d49b898b85
prakharCrudcook <prakhar@crudcook.com>
2024-08-03 17:40:23 +0530

    fix [sellerController]: added proper messaging for api and various checks

--- controllers/sellerController.js
+++ controllers/sellerController.js
@@ -4,35 +4,45 @@ const createNewToken = require('../utils/token.js');
 
 const sellerRegister = async (req, res) => {
     try {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPass = await bcrypt.hash(req.body.password, salt);
+        const { email, password, name, shopName } = req.body;
+        if(!(email && password && name && shopName)) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPass = await bcrypt.hash(req.body.password, salt);
 
-        const seller = new Seller({
-            ...req.body,
-            password: hashedPass
-        });
+            const seller = new Seller({
+                ...req.body,
+                password: hashedPass
+                // added hashed password
+            });
 
-        const existingSellerByEmail = await Seller.findOne({ email: req.body.email });
-        const existingShop = await Seller.findOne({ shopName: req.body.shopName });
+            const existingSellerByEmail = await Seller.findOne({ email: req.body.email });
+            const existingShop = await Seller.findOne({ shopName: req.body.shopName });
 
-        if (existingSellerByEmail) {
-            res.send({ message: 'Email already exists' });
-        }
-        else if (existingShop) {
-            res.send({ message: 'Shop name already exists' });
-        }
-        else {
-            let result = await Seller.create(seller);
-            result.password = undefined;
+            if (existingSellerByEmail) {
+                res.status(409).json({success: false, message: "Seller account already exists"});
+                // added proper message and api consistency
+            }
+            else if (existingShop) {
+                res.status(409).json({success: false, message: "Shop already exists"});
+                // added proper message and api consistency
+            }
+            else {
+                let result = await Seller.create(seller);
+                result.password = undefined;
 
-            const token = createNewToken(result._id)
+                const token = createNewToken(result._id)
 
-            result = {
-                ...result._doc,
-                token
-            };
-
-            res.send(result);
+                result = {
+                    ...result._doc,
+                    token
+                };
+                
+                // added proper message and api consistency
+                res.status(200).json({success: true, message: 'Seller Account Created.', result});
+            }
+        } else {
+            res.status(401).json({success: false, message: 'Please provide the necessary details for accoutn creation!'});
+            // added proper message and api consistency
         }
     } catch (err) {
         res.status(500).json(err);
@@ -54,15 +64,19 @@ const sellerLogIn = async (req, res) => {
                     token
                 };
 
-                res.send(seller);
+                res.status(200).json({success: true, message: 'Seller LogIn successful!'})
+                // added proper message and api consistency
             } else {
-                res.send({ message: "Invalid password" });
+                res.status(401).json({success: false, message: 'Invalid email or password!'});
+                // added proper message and api consistency
             }
         } else {
-            res.send({ message: "User not found" });
+            res.status(404).json({success: false, message: 'Seller account not found. Please signup.'})
+            // added proper message and api consistency
         }
     } else {
-        res.send({ message: "Email and password are required" });
+        res.status(401).json({success: false, message: 'Email and Password are required for login!'});
+        // added proper message and api consistency
     }
 };
 
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,18 @@
 const jwt = require('jsonwebtoken');
 
+const dotenv = require("dotenv")
+dotenv.config(); 
+
 const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization');
+    const token = req.header('Authorization').split(' ')[1];
 
     if (!token) {
         return res.status(401).json({ message: 'Authorization token not found' });
     }
 
     try {
-        const decoded = jwt.env(token, process.env.SECRET_KEY);
-        req.user = decoded;
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.userId = decoded;
         next();
     } catch (err) {
         return res.status(401).json({ message: 'Invalid token' });
