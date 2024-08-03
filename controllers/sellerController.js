9ac1f8952f2685ed784d32e1c1b21fdb44a7cf45
Nikhil <nikhil22322@iiitd.ac.in>
2024-08-03 14:32:48 +0530

    seller api done

--- controllers/sellerController.js
+++ controllers/sellerController.js
@@ -9,7 +9,7 @@ const sellerRegister = async (req, res) => {
 
         const seller = new Seller({
             ...req.body,
-            password: bcrypt.hash
+            password: hashedPass
         });
 
         const existingSellerByEmail = await Seller.findOne({ email: req.body.email });
@@ -51,7 +51,7 @@ const sellerLogIn = async (req, res) => {
 
                 seller = {
                     ...seller._doc,
-                    token: tokens
+                    token: token
                 };
 
                 res.send(seller);
