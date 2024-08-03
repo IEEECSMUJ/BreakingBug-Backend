1f4186035f40bde1f78e5a465c8e203a41379394
prakharCrudcook <prakhar@crudcook.com>
2024-08-03 16:07:28 +0530

    fix [utils]: fixed the way that token.js was getting SECRET_KEY

--- utils/token.js
+++ utils/token.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 const createNewToken = (payload) => {
-    return jwt.sign({ userId: payload }, process.getuid.SECRET_KEY, { expiresIn: '10d' });
+    return jwt.sign({ userId: payload }, process.env.SECRET_KEY, { expiresIn: '10d' });
 }
 
